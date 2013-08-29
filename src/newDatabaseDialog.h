cc12e4ae9a6446492f272bd97bda2ba4179fad5e
Sven Grunewaldt <strayer@olle-orks.org>
2013-08-29 21:11:50 +0200

    Licensed under GPLv3
    License change of creations_template.tex has been approved by André Hänsel

--- src/newDatabaseDialog.h
+++ src/newDatabaseDialog.h
@@ -1,3 +1,24 @@
+/*
+ * This file is part of Berichte-Ork.
+ *
+ * Berichte-Ork is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Berichte-Ork is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Berichte-Ork.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) Sven Grunewaldt, 2009
+ *
+ * Authors: Sven Grunewaldt <strayer@olle-orks.org>
+ */
+
 #ifndef NEWDATABASEDIALOG_H
 #define NEWDATABASEDIALOG_H
 
